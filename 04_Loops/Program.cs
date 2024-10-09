73ba85fb0b30f4265d30ed4a38f30092a8dbb765
Sabır Akbana <s.akbana9o@gmail.com>
2024-10-10 02:05:40 +0300

    Loops

---
+++ 04_Loops/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+
+namespace _04_Loops
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			#region For
+
+			/*
+
+			int i;
+
+			for (i = 1; i <= 5; i++)
+			{
+				Console.WriteLine(i);
+			}
+
+            for (int j = 0; j < 20; j++)
+            {
+                Console.WriteLine(j);
+            }
+
+            for (int j = 0; j < 50; j+=3)
+            {
+                Console.WriteLine(j);
+            }
+
+			Console.Write("Count : ");
+			int count = int.Parse(Console.ReadLine());
+
+            for (int j = 0; j < count; j++)
+            {
+                Console.WriteLine(j);
+            }
+
+			*/
+
+			#endregion
+
+			#region  For Loop With Decision Making
+
+			/*
+            
+            for (int i = 0; i <= 100; i++)
+            {
+                if(i%5 == 0)
+                    Console.WriteLine(i);
+            }
+
+            int total = 0;
+
+            for (int i = 0; i < 20; i++)
+            {
+                if (i % 2 == 0)
+                    total += i;
+            }
+            Console.WriteLine(total);
+
+            int count = 0;
+            for (int i = 1; i < 50; i++)
+            {
+                if (i % 7 == 0)
+                    count++;
+            }
+            Console.WriteLine(count);
+
+            int bacteria = 1;
+
+            for (int i = 1;i <= 24; i++)
+            {
+                bacteria *= 2;
+                Console.WriteLine($"{i}. Saat Sonunda : {bacteria}");
+            }
+
+            */
+
+			#endregion
+
+			#region  While
+
+			/*
+             
+            int i = 1;
+            while (i<=10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            
+            i = 1;
+            while (i <= 10)
+            {
+                if(i%3 == 0)
+                    Console.WriteLine(i);
+                i++;
+            }
+
+            */
+
+			#endregion
+
+			#region Sample App
+
+			// Calculate the sum of the digits of the 3 digit number entered from the keyboard
+
+			Console.Write("3 Digits Number : ");
+			int number = int.Parse(Console.ReadLine());
+			Console.WriteLine();
+
+			int digitCount = 0;
+			int digitTotal = 0;
+			int digitValue = 0;
+
+			while (number > 1)
+			{
+				digitValue = number % 10;
+				digitTotal += digitValue;
+				digitCount++;
+				number /= 10;
+			}
+
+			if (digitCount != 3)
+			{
+				Console.WriteLine("Please Enter 3 Digit Number\n");
+				Console.WriteLine($"Count Of The Digits = {digitCount}\n");
+			}
+			else if (number == 0)
+			{
+				Console.WriteLine("Please Enter 3 Digit Number\n");
+				Console.WriteLine($"Count Of The Digits = {digitCount + 1}\n");
+			}
+			else
+			{
+				Console.WriteLine($"Count Of The Digits = {digitCount}\n");
+				Console.WriteLine($"Sum Of The Digits = {digitTotal}");
+			}
+
+
+
+			#endregion
+
+			Console.Read();
+		}
+	}
+}
---
+++ 05_LoopWithStars/Program.cs
@@ -0,0 +1,193 @@
+﻿using System;
+
+namespace _05_LoopWithStars
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			#region Ten Star
+
+			/*
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("*");
+            }
+
+            Console.WriteLine();
+
+			for (int i = 0; i < 10; i++)
+			{
+				Console.Write("*");
+			}
+
+			*/
+
+			#endregion
+
+			#region 10 * 10 Star
+
+			/*
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+					Console.Write("*");
+				}
+                Console.WriteLine();
+            }
+
+            */
+
+			#endregion
+
+			#region Right Triangle
+
+			/*
+			 
+			for (int i = 0; i < 5; i++)
+			{
+				for (int j = 0; j <= i; j++)
+				{
+					Console.Write("*");
+				}
+				Console.WriteLine();
+			}
+
+			*/
+
+			#endregion
+
+			#region Reverse Right Triangle
+
+			/*
+			 
+			for (int i = 5; i >= 0; i--)
+			{
+				for (int j = 0; j <= i; j++)
+				{
+					Console.Write("*");
+				}
+				Console.WriteLine();
+			}
+
+			*/
+
+			#endregion
+
+			#region Symmetric Triangle
+
+			/*
+
+			for (int i = 0; i <= 5; i++)
+			{
+				for (int j = 0; j <i+1; j++)
+				{
+					Console.Write("*");
+				}
+				Console.WriteLine();
+			}
+
+			for (int i = 4; i >= 1; i--)
+			{
+				for (int j = 1; j <= i; j++)
+				{
+					Console.Write("*");
+				}
+				Console.WriteLine();
+			}
+
+			*/
+
+			#endregion
+
+			#region Baklava Slice
+
+			/*
+
+			int n = 5;
+
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = n-1; j >0; j--)
+				{
+                    Console.Write(" ");
+				}
+
+				for (int k = 1; k <= 2*i-1; k++)
+				{
+                    Console.Write("*");
+				}
+                Console.WriteLine();
+            }
+
+			for (int i = n-1; i >= 1; i--)
+			{
+				for (int j = n - 1; j > 0; j--)
+				{
+					Console.Write(" ");
+				}
+
+				for (int k = 1; k <= 2 * i - 1; k++)
+				{
+					Console.Write("*");
+				}
+				Console.WriteLine();
+			}
+
+			*/
+
+			#endregion
+
+			#region Pyramid
+
+			/*
+
+			int n = 5;
+
+			for (int i = 1; i <= n; i++)
+			{
+				for (int j = n - i; j > 0; j--)
+				{
+					Console.Write(" ");
+				}
+
+				for (int k = 1; k <= 2 * i - 1; k++)
+				{
+					Console.Write("*");
+				}
+
+				Console.WriteLine();
+			}
+
+			*/
+
+			#endregion
+
+			#region Reverse Pyramid
+
+			int n = 5;
+
+			for (int i = n; i >0 ; i--)
+			{
+				for (int j = n - i; j > 0; j--)
+				{
+					Console.Write(" ");
+				}
+
+				for (int k = 1; k <= 2 * i - 1; k++)
+				{
+					Console.Write("*");
+				}
+
+				Console.WriteLine();
+			}
+
+			#endregion
+
+			Console.Read();
+		}
+	}
+}
